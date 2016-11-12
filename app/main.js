4c73be21d916731b2c9962e928d50b4fc58663e0
AlexanderDolgan <5704737@gmai.com>
2016-11-12 13:15:01 +0300

    chapter01

--- app/main.js
+++ app/main.js
@@ -1,45 +1,6 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-//подключение компонента из библиотеки angular/core
-var core_1 = require('@angular/core');
-//указание где долно работать приложение
-var platform_browser_1 = require('@angular/platform-browser');
 //используем браузер для динамической компиляции
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-//аннотация - функция - мета дата - дополнительная информация о компоненте
-var HelloAngular = (function () {
-    function HelloAngular() {
-    }
-    HelloAngular = __decorate([
-        core_1.Component({
-            selector: 'hello-angular',
-            template: "\n        <div>\n            Hello, Angular 2!\n        </div>\n    "
-        }), 
-        __metadata('design:paramtypes', [])
-    ], HelloAngular);
-    return HelloAngular;
-}());
-//модуль
-var HelloAngularAppModule = (function () {
-    function HelloAngularAppModule() {
-    }
-    HelloAngularAppModule = __decorate([
-        core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
-            declarations: [HelloAngular],
-            bootstrap: [HelloAngular]
-        }), 
-        __metadata('design:paramtypes', [])
-    ], HelloAngularAppModule);
-    return HelloAngularAppModule;
-}());
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(HelloAngularAppModule);
+var app_module_1 = require('./app.module');
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.HelloAngularAppModule);
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -1,30 +1,6 @@
-//подключение компонента из библиотеки angular/core
-import { Component, NgModule } from '@angular/core';
-
-//указание где долно работать приложение
-import { BrowserModule } from '@angular/platform-browser';
-
 //используем браузер для динамической компиляции
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-//аннотация - функция - мета дата - дополнительная информация о компоненте
-@Component({
-    selector: 'hello-angular',
-    template: `
-        <div>
-            Hello, Angular 2!
-        </div>
-    `
-})
-//класс
-class HelloAngular {}
-
-//модуль
-@NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ HelloAngular ],
-    bootstrap: [ HelloAngular ]
-})
-class HelloAngularAppModule {}
+import { HelloAngularAppModule } from './app.module'
 
 platformBrowserDynamic().bootstrapModule(HelloAngularAppModule);
