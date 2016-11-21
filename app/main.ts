4bb774f308bcf39b616296b928bcdadded49c9c4
AlexanderDolgan <5704737@gmai.com>
2016-11-21 09:30:19 +0300

    :fire:

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,1 @@
-//используем браузер для динамической компиляции
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { HelloAngularAppModule } from './app.module'
-
-platformBrowserDynamic().bootstrapModule(HelloAngularAppModule);
